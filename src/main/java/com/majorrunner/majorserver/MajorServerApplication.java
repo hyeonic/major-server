73bd8be509018a7c4a910e9be1f14acc3b2bd726
hyeonic <evan3566@gmail.com>
2020-09-21 14:17:40 +0900

    feat: Project setting
    Spring web
    Spring JPA
    Spring Devtools
    Spring HATEOAS
    Spring Rest Docs
    Lombok
    mysql database
    h2 database (test)

---
+++ src/main/java/com/majorrunner/majorserver/MajorServerApplication.java
@@ -0,0 +1,13 @@
+package com.majorrunner.majorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/majorrunner/majorserver/MajorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorrunner.majorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
